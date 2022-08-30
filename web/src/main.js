7eabea7697a51a68d770a0dcef62871ece123c2e
andyhslam <42641473+andyhslam@users.noreply.github.com>
2022-08-30 08:26:10 +0800

    node-vue-moba-3.7-首页顶部轮播图片(vue swiper)

--- web/src/main.js
+++ web/src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue"
 import App from "./App.vue"
-import router from "./router"
-import "./assets/css/style.scss"
-
 Vue.config.productionTip = false
+import "./assets/scss/style.scss"
+import router from "./router"
+import VueAwesomeSwiper from "vue-awesome-swiper"
+import "swiper/dist/css/swiper.css"
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
 	router,
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,12 +1,58 @@
 <template>
-	<div class="home">首页</div>
+	<div class="home">
+		<swiper :options="swiperOption">
+			<swiper-slide>
+				<img
+					class="w-100"
+					:src="require('@/assets/images/top_banner1.jpeg')"
+				/>
+			</swiper-slide>
+			<swiper-slide>
+				<img
+					class="w-100"
+					:src="require('@/assets/images/top_banner2.jpeg')"
+				/>
+			</swiper-slide>
+			<swiper-slide>
+				<img
+					class="w-100"
+					:src="require('@/assets/images/top_banner3.jpeg')"
+				/>
+			</swiper-slide>
+			<div
+				class="swiper-pagination home-pagination text-right px-3 pb-1"
+				slot="pagination"
+			></div>
+		</swiper>
+	</div>
 </template>
 
 <script>
 export default {
 	name: "HomeView",
 	components: {},
+	data() {
+		return {
+			swiperOption: {
+				pagination: {
+					el: ".home-pagination",
+				},
+			},
+		}
+	},
 }
 </script>
+<style lang="scss" scoped>
+@import "@/assets/scss/_variables.scss";
 
-<style></style>
+.home-pagination {
+	.swiper-pagination-bullet {
+		opacity: 1;
+		border-radius: 0.1538rem;
+		background-color: map-get($colors, "white");
+		&.swiper-pagination-bullet-active {
+			background-color: map-get($colors, "info");
+		}
+	}
+}
+</style>
